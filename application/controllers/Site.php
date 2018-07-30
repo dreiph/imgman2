4cb654207182dd3365a57d82fafd08130cb2292d
dreiph <41894095+dreiph@users.noreply.github.com>
2018-07-30 17:00:31 +0300

    start

--- application/controllers/Site.php
+++ application/controllers/Site.php
@@ -2,24 +2,133 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Site extends CI_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+        public function __construct()
+        {
+                parent::__construct();
+                session_start();
+        }
 	public function index()
 	{
-		$this->load->view('intro');
+		// $this->output->enable_profiler(true);
+		$data=array();
+		
+		//pagination
+		$this->load->library('pagination');
+		$config['full_tag_open'] = "<ul class='uk-pagination'>";
+		$config['full_tag_close'] ="</ul>";
+		$config['num_tag_open'] = '<li>';
+		$config['num_tag_close'] = '</li>';
+		$config['cur_tag_open'] = "<li class='uk-active'><span>";
+		$config['cur_tag_close'] = "</span></li>";
+		$config['next_tag_open'] = "<li>";
+		$config['next_tagl_close'] = "</li>";
+		$config['prev_tag_open'] = "<li>";
+		$config['prev_tagl_close'] = "</li>";
+		$config['first_tag_open'] = "<li>";
+		$config['first_tagl_close'] = "</li>";
+		$config['last_tag_open'] = "<li>";
+		$config['last_tagl_close'] = "</li>";
+		
+		$config['first_link'] = 'Pirmas';
+		$config['last_link'] = 'Paskutinis';
+		
+		$config['base_url'] = base_url().'site/index/';
+		$config['per_page'] = 2;
+		$config["uri_segment"] = 3;
+		$page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+
+		if(!isset($_SESSION['order']))
+		{
+			$_SESSION['order']='date';
+		}				
+		if(!isset($_SESSION['sort']))
+		{
+			$_SESSION['sort']='desc';
+		}
+		
+		if(isset($_SESSION['sq']))
+		{
+			$q=$this->db->query("SELECT * FROM images WHERE img_dimensions LIKE '%".$_SESSION['sq']."%' OR img_upload_filename LIKE '%".$_SESSION['sq']."%' OR img_system_filename LIKE '%".$_SESSION['sq']."%' ORDER BY ".$_SESSION['order']." ".$_SESSION['sort']." LIMIT ?, ?", array((int)$page,(int)$config['per_page']));
+			
+			$config['total_rows'] = $this->db->query("SELECT uid FROM images WHERE img_dimensions LIKE '%".$_SESSION['sq']."%' OR img_upload_filename LIKE '%".$_SESSION['sq']."%' OR img_system_filename LIKE '%".$_SESSION['sq']."%'")->num_rows();
+		}
+		else
+		{
+			$q=$this->db->query("SELECT * FROM images WHERE 1=1 ORDER BY ".$_SESSION['order']." ".$_SESSION['sort']." LIMIT ?, ?", array((int)$page,(int)$config['per_page']));
+			
+			$config['total_rows'] = $this->db->query("SELECT uid FROM images WHERE 1=1")->num_rows();
+		}
+
+		$this->pagination->initialize($config);
+
+		$data['links']=$this->pagination->create_links();
+		
+		if($q->num_rows()>0)
+		{
+			$data['results']=$q->result();
+		}
+		else
+		{
+			$data['error']="No images yet";
+		}
+		
+		$this->load->view('intro', $data);
+	}
+	
+	public function action($action)
+	{
+		if($action=='date_asc')
+		{
+			$_SESSION['order']='datetime_uploaded';
+			$_SESSION['sort']='asc';
+		}
+		if($action=='date_desc')
+		{
+			$_SESSION['order']='datetime_uploaded';
+			$_SESSION['sort']='desc';
+		}
+		redirect('site/index','refresh');
+	}
+	public function search()
+	{
+		if(isset($_POST['sq']))
+		{
+			if($_POST['sq']<>""){
+				$_SESSION['sq']=$this->input->post('sq');
+			}
+			else
+			{
+				if(isset($_SESSION['sq']))
+				{
+					unset($_SESSION['sq']);
+				}
+			}
+		}
+		redirect('site/index','refresh');
+	}
+	
+	public function upload()
+	{
+		var_dump($_FILES);
+		$config['upload_path']          = './uploads/';
+		$config['allowed_types']        = 'gif|jpg|png';
+		$config['max_size']             = 10000;
+		$config['max_width']            = 1024;
+		$config['max_height']           = 768;
+
+		$this->load->library('upload', $config);
+
+		if ( ! $this->upload->do_upload('userfile'))
+		{
+				$error = array('error' => $this->upload->display_errors());
+
+				$this->load->view('upload_form', $error);
+		}
+		else
+		{
+				$data = array('upload_data' => $this->upload->data());
+
+				$this->load->view('upload_success', $data);
+		}
 	}
 }
--- application/views/intro.php
+++ application/views/intro.php
@@ -7,16 +7,180 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	
-	<title>test</title>
+	<title>Imgman</title>
 	
-	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
-	<link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon.png">
-	<link rel="stylesheet" href="css/docs.css">
+	<base href="<?php echo base_url(); ?>" />
 	
-	<script src="../vendor/jquery.js"></script>
-	<script src="js/docs.js"></script>
+	<link rel="shortcut icon" href="public/img/favicon.ico" type="image/x-icon">
+
+	<link rel="stylesheet" href="public/uikit/css/uikit.min.css">
+	<link rel="stylesheet" href="public/uikit/css/style.css">
+	<link rel="stylesheet" href="public/uikit/css/components/slidenav.min.css">
+	<link rel="stylesheet" href="public/uikit/css/components/placeholder.min.css">
+	<link rel="stylesheet" href="public/uikit/css/components/form-file.min.css">
+	
+	<script src="public/jq/jquery-3.3.1.min.js"></script>
+	<script src="public/uikit/js/uikit.min.js"></script>
+	<script src="public/uikit/js/components/lightbox.min.js"></script>
+	<script src="public/uikit/js/components/upload.min.js"></script>
 </head>
 
 <body>
+	<div class="uk-container uk-container-center">
+		<div class="uk-grid">
+			<div class="uk-width-1-1">
+				<div class="logo">
+					<div class="logomenu uk-align-left">
+						<img src="public/img/imgman.png" alt="Logo">
+					</div>
+					<div class="uk-text-right">
+						<a href="" class="uk-button">
+							<i class="uk-icon-user"></i> Logout
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div class="uk-grid">
+			<div class="uk-width-1-1">
+			<div class="filter">
+				<div class="uk-align-left">
+					<form class="uk-form uk-form-horizontal" action="site/search" method="post">
+					Filter by:
+					<a href="site/action/date_asc" class="uk-button uk-button-primary <?php if($_SESSION['order']=='datetime_uploaded' && $_SESSION['sort']=='asc'){ echo 'filter_marked'; } ?>">
+						Date ASC <i class="uk-icon-sort-alpha-asc"></i>
+					</a>
+					<a href="site/action/date_desc" class="uk-button uk-button-primary <?php if($_SESSION['order']=='datetime_uploaded' && $_SESSION['sort']=='desc'){ echo 'filter_marked'; } ?>">
+						Date DESC <i class="uk-icon-sort-alpha-desc"></i>
+					</a>
+					<input type="text" name="sq" value="<?php if(isset($_SESSION['sq'])){ echo $_SESSION['sq']; } ?>" placeholder="Enter search query" class="uk-input" />
+					<button class="uk-button uk-button-primary">
+						<i class="uk-icon-search"></i> Search
+					</button>
+					</form>
+				</div>
+				<div class="uk-text-right">
+					<a class="uk-button uk-button-success">
+						<i class="uk-icon-plus"></i> Upload
+					</a>
+				</div>
+			</div>
+			</div>
+		</div>
+		
+		<div class="uk-grid">
+			<div class="uk-width-1-1">
+			<?php if(isset($error)): ?>
+			<div class="uk-alert uk-alert-danger">
+			<h6><?php echo $error; ?></h6>
+			</div>
+			<?php else: ?>
+			<div class="uk-grid results">	
+				<div class="uk-width-1-1">
+				<table class="uk-table uk-table-striped">
+				<tr>
+					<th>Image</th>
+					<th>Dimensions</th>
+					<th>Original filename</th>
+					<th>Path to CDN image</th>
+					<th>Upload date</th>
+				</tr>
+				<?php foreach($results as $r): ?>
+				<tr>
+					<td>
+						<a href="<?php echo $r->img_system_filename; ?>" data-uk-lightbox="{group:'my-group'}" title="<?php echo base_url(); ?><?php echo $r->img_system_filename; ?> ">
+						<img  class="uk-thumbnail" src="<?php echo $r->img_system_filename; ?>" data-id="<?php echo $r->uid; ?>" />
+						</a>
+					</td>
+					<td>
+						<?php echo $r->img_dimensions; ?>
+					</td>
+					<td>
+						<?php echo $r->img_upload_filename; ?>
+					</td>
+					<td>
+						<?php echo base_url(); ?><?php echo $r->img_system_filename; ?> 
+						<a href="<?php echo base_url(); ?><?php echo $r->img_system_filename; ?>" target="_blank" title="Open in new tab">
+							<i class="uk-icon-arrow-right"></i>
+						</a>
+					</td>
+					<td>
+						<?php echo $r->datetime_uploaded; ?>
+					</td>
+				</tr>
+				<?php endforeach; ?>
+				</table>
+				</div>
+			</div>
+			<?php endif; ?>
+			</div>
+		</div>
+		
+		<div class="uk-grid">
+			<div class="uk-width-1-1">
+			<div class="pagination">
+			<?php if(isset($links)){ echo $links; } ?>
+			</div>
+			</div>
+		</div>
+		
+		<div class="uk-grid">
+			<div class="uk-width-1-1">
+			<div class="footer">&copy 2018 dreiph.com</div>
+			</div>
+		</div>
+	</div>
+	
+
+
+<div id="progressbar" class="uk-progress uk-hidden">
+    <div class="uk-progress-bar" style="width: 0%;">...</div>
+</div>
+
+<div id="upload-drop" class="uk-placeholder uk-text-center">
+<i class="uk-icon-cloud-upload uk-icon-medium uk-text-muted uk-margin-small-right"></i> Attach binaries by dropping them here or <a class="uk-form-file">selecting one<input id="upload-select" type="file"></a>.
+</div>
+
+<script>
+
+    $(function(){
+
+        var progressbar = $("#progressbar"),
+            bar         = progressbar.find('.uk-progress-bar'),
+            settings    = {
+
+            action: '<?php echo base_url(); ?>site/upload', // upload url
+
+            allow : '*.(jpg|jpeg|gif|png)', // allow only images
+
+            loadstart: function() {
+                bar.css("width", "0%").text("0%");
+                progressbar.removeClass("uk-hidden");
+            },
+
+            progress: function(percent) {
+                percent = Math.ceil(percent);
+                bar.css("width", percent+"%").text(percent+"%");
+            },
+
+            allcomplete: function(response) {
+
+                bar.css("width", "100%").text("100%");
+
+                setTimeout(function(){
+                    progressbar.addClass("uk-hidden");
+                }, 250);
+
+                alert("Upload Completed")
+            }
+        };
+
+        var select = UIkit.uploadSelect($("#upload-select"), settings),
+            drop   = UIkit.uploadDrop($("#upload-drop"), settings);
+    });
+
+</script>
+							
 </body>
 </html>
