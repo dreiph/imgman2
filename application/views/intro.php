6d71d9ea5b05ffc1dab5c4906da49f8ea6a3f642
dreiph <41894095+dreiph@users.noreply.github.com>
2018-07-30 12:52:00 +0300

    start

---
+++ application/views/intro.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en-gb" dir="ltr">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title>test</title>
+	
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon-precomposed" href="images/apple-touch-icon.png">
+	<link rel="stylesheet" href="css/docs.css">
+	
+	<script src="../vendor/jquery.js"></script>
+	<script src="js/docs.js"></script>
+</head>
+
+<body>
+</body>
+</html>
